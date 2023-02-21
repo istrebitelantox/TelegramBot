27752ed7496961c7811472347a94ab3982f44d8e
istrebitelantox <geekpook@gmail.com>
2023-02-21 17:27:59 +0300

    Dumb, but works

---
+++ src/main/java/org/example/Bot.java
@@ -0,0 +1,94 @@
+package org.example;
+
+import org.telegram.telegrambots.bots.TelegramLongPollingBot;
+import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
+import org.telegram.telegrambots.meta.api.objects.Message;
+import org.telegram.telegrambots.meta.api.objects.Update;
+import org.telegram.telegrambots.meta.api.objects.replykeyboard.ReplyKeyboardMarkup;
+import org.telegram.telegrambots.meta.api.objects.replykeyboard.buttons.KeyboardButton;
+import org.telegram.telegrambots.meta.api.objects.replykeyboard.buttons.KeyboardRow;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
+
+import java.util.ArrayList;
+
+public class Bot extends TelegramLongPollingBot {
+    //создаем две константы, присваиваем им значения токена и имя бота соответсвтенно
+    //вместо звездочек подставляйте свои данные
+    final private String BOT_TOKEN = "6230654907:AAFJNsjTrLDqBhQbAe0zXaY0-qmmX15Jsaw";
+    final private String BOT_NAME = "FirstBot";
+    Storage storage;
+    ReplyKeyboardMarkup replyKeyboardMarkup;
+
+    Bot()
+    {
+        storage = new Storage();
+        initKeyboard();
+    }
+
+    @Override
+    public String getBotUsername() {
+        return BOT_NAME;
+    }
+
+    @Override
+    public String getBotToken() {
+        return BOT_TOKEN;
+    }
+
+    @Override
+    public void onUpdateReceived(Update update) {
+        try{
+            if(update.hasMessage() && update.getMessage().hasText())
+            {
+                //Извлекаем из объекта сообщение пользователя
+                Message inMess = update.getMessage();
+                //Достаем из inMess id чата пользователя
+                String chatId = inMess.getChatId().toString();
+                //Получаем текст сообщения пользователя, отправляем в написанный нами обработчик
+                String response = parseMessage(inMess.getText());
+                //Создаем объект класса SendMessage - наш будущий ответ пользователю
+                SendMessage outMess = new SendMessage();
+
+                //Добавляем в наше сообщение id чата а также наш ответ
+                outMess.setChatId(chatId);
+                outMess.setText(response);
+                outMess.setReplyMarkup(replyKeyboardMarkup);
+
+                //Отправка в чат
+                execute(outMess);
+            }
+        } catch (TelegramApiException e) {
+            e.printStackTrace();
+        }
+    }
+    public String parseMessage(String textMsg) {
+        String response;
+
+        //Сравниваем текст пользователя с нашими командами, на основе этого формируем ответ
+        if(textMsg.equals("/start"))
+            response = "Приветствую, бот знает много анекдотов. Жми \"Посмеятся\", чтобы получить случайный из них";
+        else if(textMsg.equals("/get") || textMsg.equals("Посмеяться"))
+            response = storage.getRandQuote();
+        else
+            response = "Сообщение не распознано";
+
+        return response;
+    }
+    void initKeyboard()
+    {
+        //Создаем объект будущей клавиатуры и выставляем нужные настройки
+        replyKeyboardMarkup = new ReplyKeyboardMarkup();
+        replyKeyboardMarkup.setResizeKeyboard(true); //подгоняем размер
+        replyKeyboardMarkup.setOneTimeKeyboard(false); //скрываем после использования
+
+        //Создаем список с рядами кнопок
+        ArrayList<KeyboardRow> keyboardRows = new ArrayList<>();
+        //Создаем один ряд кнопок и добавляем его в список
+        KeyboardRow keyboardRow = new KeyboardRow();
+        keyboardRows.add(keyboardRow);
+        //Добавляем одну кнопку с текстом "Посмеяться" наш ряд
+        keyboardRow.add(new KeyboardButton("Посмеяться"));
+        //добавляем лист с одним рядом кнопок в главный объект
+        replyKeyboardMarkup.setKeyboard(keyboardRows);
+    }
+}
---
+++ src/main/java/org/example/Storage.java
@@ -0,0 +1,47 @@
+package org.example;
+
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import  org.jsoup.select.Elements;
+
+import java.util.ArrayList;
+import java.io.IOException;
+
+public class Storage {
+    private ArrayList<String> quoteList;
+    Storage()
+    {
+        quoteList = new ArrayList<>();
+        parser("https://www.mk.ru/anekdoti/");
+//        quoteList.add("Начинать всегда стоит с того, что сеет сомнения. \n\nБорис Стругацкий.");
+//        quoteList.add("80% успеха - это появиться в нужном месте в нужное время.\n\nВуди Аллен");
+//        quoteList.add("Мы должны признать очевидное: понимают лишь те,кто хочет понять.\n\nБернар Вербер");
+    }
+
+    String getRandQuote()
+    {
+        //получаем случайное значение в интервале от 0 до самого большого индекса
+        int randValue = (int)(Math.random() * quoteList.size());
+        //Из коллекции получаем цитату со случайным индексом и возвращаем ее
+        return quoteList.get(randValue);
+    }
+    void parser(String strURL)
+    {
+        String className = "listing-preview__desc";
+        Document doc = null;
+        try {
+            //Получаем документ нужной нам страницы
+            doc = Jsoup.connect(strURL).maxBodySize(0).get();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        //Получаем группу объектов, обращаясь методом из Jsoup к определенному блоку
+        Elements elQuote = doc.getElementsByClass(className);
+
+        //Достаем текст из каждого объекта поочереди и добавляем в наше хранилище
+        elQuote.forEach(el -> {
+            quoteList.add(el.text());
+        });
+    }
+}
